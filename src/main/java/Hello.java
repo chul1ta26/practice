2c606760b598d698f3cf2f5603da15f5b890b37f
chul1ta26 <dayhany26@gmail.com>
2019-09-23 22:43:36 -0400

    adding a comment

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
    public static void main(String[] args) {
        System.out.println("Hello World!");
-       System.out.println("How are you");
+       System.out.println("How are you");//going back to basics
 
     }
 }
